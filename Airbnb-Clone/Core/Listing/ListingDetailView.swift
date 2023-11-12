deee91cb1fcacf9fca540ed953c1631300f3eb9c
piwien <berkayyasar01@gmail.com>
2023-11-12 16:57:11 +0300

    listingdetailview design first part

--- Airbnb-Clone/Core/Listing/ListingDetailView.swift
+++ Airbnb-Clone/Core/Listing/ListingDetailView.swift
@@ -38,6 +38,60 @@ struct ListingDetailView: View {
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             .padding(.horizontal)
+            
+            Divider()
+            
+           // host info
+            HStack {
+                VStack(alignment: .leading, spacing: 4){
+                    Text("Entire villa hosted by John Smith")
+                        .font(.headline)
+                        .frame(width: 250, alignment: .leading)
+                    
+                    HStack(spacing: 2) {
+                        Text("4 guests - ")
+                        Text("4 bedrooms - ")
+                        Text("4 beds - ")
+                        Text("3 baths")
+                    }
+                    .font(.caption)
+                }
+                .frame(width: 300, alignment: .leading)
+                
+                Spacer()
+                
+                Image("female-profile-photo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 70, height: 70)
+                    .clipShape(Circle())
+            }
+            .padding(.horizontal)
+            .padding(.vertical, 5)
+            
+            Divider()
+            
+            // listing features
+            
+            VStack(alignment: .leading, spacing: 15) {
+                ForEach(0..<2) { feature in
+                    HStack(spacing: 12) {
+                        Image(systemName: "medal")
+                            
+                        VStack(alignment: .leading) {
+                            Text("Superhost")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                            
+                            Text("Superhosts are experience, highly rated hosts who are commited to providing great stars for guests")
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        }
+                        Spacer()
+                    }
+                }
+            }
+            .padding()
         }
     }
 }
